760d7d1b31216b03771ef5e4f4ffeb06f602d519
Prokurator <fiordi@yandex.ru>
2020-11-23 10:38:16 +0400

    Edit ApiRoutes

--- root/{{entity.name}}/routes/{{entity.nameUpper}}ApiRoutes.java
+++ root/{{entity.name}}/routes/{{entity.nameUpper}}ApiRoutes.java
@@ -3,7 +3,7 @@ package {{path}}.{{entity.name}}.routes;
 import {{path}}.base.routes.BaseApiRoutes;
 
 public class {{entity.nameUpper}}ApiRoutes {
-    public static final String ADMIN = BaseApiRoutes.ADMIN + "/{{entity.name}}";
+    public static final String ADMIN = BaseApiRoutes.ADMIN + "/{{entity.restApiUrl}}";
     public static final String ADMIN_BY_ID = ADMIN + "/{id}";
     public static final String ROOT = BaseApiRoutes.V1 + "/{{entity.name}}";
     public static final String BY_ID = ROOT + "/{id}";
